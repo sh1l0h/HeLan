6ba3c08213feb969387abc31fb9d1205715fa1a4
Mirian-Shilakadze <mirian.shilakadze@gmail.com>
2021-12-21 23:09:26 +0400

    Add license

--- src/ge/helan/main/Main.java
+++ src/ge/helan/main/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
         HeLan h = null;
         try {
-            h = new HeLan(new String ( Files.readAllBytes( Paths.get("/home/mirian/HeLan/test.hl") ) ));
+            h = new HeLan(new String ( Files.readAllBytes( Paths.get(args[0]) ) ));
         } catch (IOException e) {
             e.printStackTrace();
         }
